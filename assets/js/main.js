61edb9052f2bd948cc8dd09f3b5ceb2c6c6fd62f
sskkfmnn <sskkfmnn@gmail.com>
2023-02-20 15:50:35 +0100

    Lev1_3

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+console.log("");
+
+function intro3(name, stadt, alter){
+    console.log("Hallo, mein Name ist " + name + ". Ich bin " + alter + ". Ich komme aus " + stadt + ".")
+}
+
+intro3 ('SuperCoder', 'Düsseldorf', '27');
